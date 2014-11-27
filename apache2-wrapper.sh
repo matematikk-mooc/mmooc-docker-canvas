7f4176f704421795547dcf1baae9804dc5aba4c3
Kjell-Magne Øierud <kjellm@oierud.net>
2014-11-27 20:48:08 +0100

    Set encryption key on first run

--- apache2-wrapper.sh
+++ apache2-wrapper.sh
@@ -13,4 +13,7 @@ dirs="/opt/canvas-lms/log /opt/canvas-lms/tmp/files"
 chown canvasuser $dirs
 chmod 0777 $dirs
 
+# https://github.com/instructure/canvas-lms/wiki/Production-Start#security-configuration
+ruby -rsecurerandom -pi -e '$_.sub!("@@", SecureRandom.base64(20).inspect)' /opt/canvas-lms/config/security.yml
+
 exec /usr/sbin/apache2 -D FOREGROUND
